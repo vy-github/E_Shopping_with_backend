6ceacb56ea249f2728641e3ae354b76bedf8c6d8
Vaibhav Yeole <vaibhav.yeole159@gmail.com>
2021-09-24 16:31:37 +0530

    item populated

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,31 @@
 import React from "react";
+// import images from "../../../public/upload/image.jpg";
 
 const Item = (props) => {
-  const { title, description, price } = props.item;
+  const { title, description, price, category, image } = props.item;
   return (
     <>
-      <div>
-        <h2>{title}</h2>
-        <h2>{description}</h2>
-        <h2>{price}</h2>
+      <div className="card">
+        <div className="image-area">
+          <img
+            src="https://source.unsplash.com/300x300/?products"
+            alt="not found"
+          />
+        </div>
+
+        <div className="content-area">
+          <div className="title-desription-area">
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </div>
+
+          <div className="bottom-area">
+            <p>Rs. {price}</p>
+            <div className="button-area">
+              <button>Add to cart</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
